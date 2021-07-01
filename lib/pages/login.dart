5da05a0917c65edcc274caa606cb72f73bee8b89
Sampath Balivada <balivadask2000@gmail.com>
2021-07-01 18:20:39 +0530

    modified login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -11,8 +11,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController emailId = TextEditingController();
-  TextEditingController password = TextEditingController();
+  TextEditingController signInEmailId = TextEditingController();
+  TextEditingController signInPassword = TextEditingController();
+
+  TextEditingController signUpEmailId = TextEditingController();
+  TextEditingController signUpPassword = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +24,100 @@ class _LoginPageState extends State<LoginPage> {
         title: Text('Vignan\'s Institute of Information Technology'),
       ),
       body: Center(
-        child: Column(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            TextField(
-              controller: emailId,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                hintText: 'E-Mail ID',
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 60, right: 30),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Sign In'),
+                    buildCustomTextField(
+                      signInEmailId,
+                      TextInputType.emailAddress,
+                      'Email-ID',
+                    ),
+                    buildCustomTextField(
+                      signInPassword,
+                      TextInputType.visiblePassword,
+                      'Password',
+                      obscureText: true,
+                    ),
+                    ElevatedButton(
+                      child: Text('Login'),
+                      onPressed: () async {
+                        bool r = await widget.model.signIn(
+                          signInEmailId.text,
+                          signInPassword.text,
+                        );
+
+                        if (r) {
+                          // goto HomePage
+                        } else {
+                          // show Dialog box with reason for error
+                        }
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
-            TextField(
-              controller: password,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-            ),
-            ElevatedButton(
-              child: Text('Login'),
-              onPressed: () async {
-                bool r = await widget.model.signIn(emailId.text, password.text);
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30, right: 60),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Register'),
+                    buildCustomTextField(
+                      signUpEmailId,
+                      TextInputType.emailAddress,
+                      'Email-ID',
+                    ),
+                    buildCustomTextField(
+                      signUpPassword,
+                      TextInputType.visiblePassword,
+                      'Password',
+                      obscureText: true,
+                    ),
+                    ElevatedButton(
+                      child: Text('Register'),
+                      onPressed: () async {
+                        bool r = await widget.model.signUp(
+                          signUpEmailId.text,
+                          signUpPassword.text,
+                        );
 
-                print(r);
-              },
+                        if (r) {
+                          // show Dialog Box to verify email
+                        } else {
+                          // show Dialog Box to contact admin at college
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  TextField buildCustomTextField(TextEditingController controller,
+      TextInputType inputType, String hintText,
+      {bool obscureText = false}) {
+    return TextField(
+      controller: controller,
+      keyboardType: inputType,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
         ),
       ),
     );
