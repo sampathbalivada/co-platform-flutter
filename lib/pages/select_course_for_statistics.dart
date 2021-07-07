3e437020a164ec448bc992926c00cad0201be797
Sampath Balivada <balivadask2000@gmail.com>
2021-07-07 17:19:13 +0530

    added two pages

---
+++ lib/pages/select_course_for_statistics.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:co_attainment_platform/models/model.dart';
+
+class SelectCourseForStatistics extends StatefulWidget {
+  final model;
+
+  SelectCourseForStatistics({@required this.model});
+
+  @override
+  _SelectCourseForStatisticsState createState() => _SelectCourseForStatisticsState();
+}
+
+class _SelectCourseForStatisticsState extends State<SelectCourseForStatistics> {
+  List<Widget> generateExpansionTiles() {
+    Map<String, List<Course>> coursesAssigned = widget.model.coursesAssigned;
+
+    List<Widget> expansionTiles = [];
+
+    for (var batch in coursesAssigned.keys) {
+      expansionTiles.add(
+        ExpansionTile(
+          title: Text(
+            "Batch " + batch,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: coursesAssigned[batch]?.length,
+              itemBuilder: (context, index) {
+                var course = coursesAssigned[batch]?[index];
+
+                return OutlinedButton(
+                  style: ButtonStyle(
+                    alignment: Alignment.centerLeft,
+                  ),
+                  onPressed: () {
+                    widget.model.setCurrentCourse(course);
+                    Navigator.pushNamed(
+                      context,
+                      '/select_mid_and_check_statistics',
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      course?.courseName,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      );
+    }
+
+    return expansionTiles;
+  }
+
+  List<Widget> generateExpansionTileWidgets(List<Course>? courses) {
+    List<Widget> expansionTileWidgets = [];
+
+    for (var course in courses ?? []) {
+      expansionTileWidgets.add(
+        OutlinedButton(
+          style: ButtonStyle(
+            alignment: Alignment.centerLeft,
+          ),
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              course.courseName,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return expansionTileWidgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home > Upload Marks'),
+      ),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.25,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: generateExpansionTiles(),
+          ),
+        ),
+      ),
+    );
+  }
+}
